de641124ba09d2ac06a02c3059ee456b33f3621a
ChrisDizenzo <42619685+ChrisDizenzo@users.noreply.github.com>
2020-06-13 11:51:58 -0400

    fixieboi

---
+++ server/server.js
@@ -0,0 +1,38 @@
+const port = 7070;
+const host = '127.0.0.1';
+
+const express = require('express')
+const app = express();
+const server = require('http').Server(app);
+const io = require("socket.io")(server);
+const moment = require('moment');
+
+
+app.use(express.json())
+
+server.listen(port);
+console.log("listening on port: " + port)
+
+app.get('/', (req, res) => {
+	res.send('<h1>Hello world</h1>');
+	// console.log(req.headers)
+});
+
+io.on('connection',(socket) => {
+    socket.on('makeRoom', (data)=>{
+        console.log('You made a creebin!')
+        console.log(JSON.stringify(data))
+    })
+
+    socket.on('joinRoom', (data)=>{
+        console.log('You joined creebo!')
+        console.log(JSON.stringify(data))
+
+    })
+
+    socket.on('sendAva', (data)=>{
+        console.log('You sent creebin!')
+        console.log(JSON.stringify(data))
+    })
+
+})
--- src/main.js
+++ src/main.js
@@ -6,10 +6,21 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 import { faCaretLeft, faCaretRight } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { formatDistance, subDays } from 'date-fns'
+import VueSocketIO from 'vue-socket.io'
+import SocketIO from "socket.io-client"
 
 Vue.component(formatDistance)
 Vue.component(subDays)
-
+Vue.use(new VueSocketIO({
+  debug: true,
+  connection: SocketIO('http://172.6.249.239:4000'), //options object is Optional
+  vuex: {
+    store,
+    actionPrefix: "SOCKET_",
+    mutationPrefix: "SOCKET_"
+  }
+})
+);
  
 
 library.add(faCaretRight)
